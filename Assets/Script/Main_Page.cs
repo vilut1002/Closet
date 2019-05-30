e2fb3e2bc160de11146ed5a4bde5cd1c62b2b760
vilut1002 <45375130+vilut1002@users.noreply.github.com>
2019-05-31 05:54:43 +0900

    Initial commit

---
+++ Assets/Script/Main_Page.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Main_Page : MonoBehaviour
+{
+    public Image menu_bar, main_bg, menu_bttn, basic_menu, main_bg_dark, cart_page;
+    public RawImage product;
+    public GameObject main_page, cart_scrollview;
+    public Image mypage;
+    public GameObject[] product_page;
+    public GameObject curr_page;
+    public GameObject[] cart_elements; //카트 요소들
+    public ArrayList cart_content = new ArrayList(); //장바구니에 담긴 상품 리스트
+    public bool Is_menu_open;
+
+    Stack<GameObject> page = new Stack<GameObject>();
+
+    void Start() {
+        Is_menu_open = false;
+        menu_bar.gameObject.SetActive(false);
+        main_bg.gameObject.SetActive(true);
+        menu_bttn.gameObject.SetActive(true);
+        main_bg_dark.gameObject.SetActive(false);
+        mypage.gameObject.SetActive(false);
+        cart_page.gameObject.SetActive(false);
+       // get_Image();
+        curr_page = main_page;
+        main_page.SetActive(true);
+    }
+
+    //왼쪽 메뉴바 열기
+    public void menu_open() {
+        if (Is_menu_open)
+        {
+            menu_close();
+        }
+        else
+        {
+            menu_bar.gameObject.SetActive(true);
+            main_bg_dark.gameObject.SetActive(true);
+            Is_menu_open = true;
+        }
+        }
+
+    //왼쪽 메뉴바 닫기
+    public void menu_close() {
+        menu_bar.gameObject.SetActive(false);
+        main_bg_dark.gameObject.SetActive(false);
+        Is_menu_open = false;
+    }
+
+    //마이페이지 버튼 눌렀을 때
+    public void my_page()
+    {
+        change_page(mypage.gameObject);
+
+    }
+
+    //뒤로가기 버튼 눌렀을때 이전 화면으로 바꾸기
+    public void back_prev_page()
+    {
+        change_page(page.Pop());
+    }
+
+    //상품 페이지로 이동하기
+    public void go_product_detail()
+    {
+
+    }
+
+    //화면 전환
+    public void change_page(GameObject new_page)
+    {
+        curr_page.SetActive(false);
+        new_page.SetActive(true);
+        curr_page = new_page;
+        page.Push(curr_page);
+    }
+
+    //장바구니 화면으로 가기
+    public void go_cart()
+    {
+        change_page(cart_page.gameObject);
+        /*cart_scrollview.GetComponent<RectTransform>().sizeDelta = new Vector2(500, cart_content.Count*200);
+        IEnumerator cart_list = cart_content.GetEnumerator();
+        int i = 0;
+        while (cart_list.MoveNext())
+        {
+            cart_elements[i] = (GameObject)cart_list.Current;
+        }
+        */
+    }
+
+    
+
+    //장바구니 담기
+    public void into_cart()
+    {
+
+    }
+
+    //상품 정보 얻기
+    public void Get_prod_info()
+    {
+      
+    }
+
+
+    /*
+    public string url = "https://community.arm.com/cfs-file/__key/communityserver-blogs-components-weblogfiles/00-00-00-20-66/6180.mali_2D00_shield_2D00_2.png";
+
+    public void get_Image() {
+        GetTexture();
+    }
+
+    IEnumerator GetTexture()
+    {
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            product.texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        }
+    }
+    */
+
+
+
+
+
+}
