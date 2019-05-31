b50a0a05aa725c827bbc4a7fd691d86488cf5808
vilut1002 <45375130+vilut1002@users.noreply.github.com>
2019-05-31 10:09:24 +0900

    0531
    menu bar animation

--- Assets/Script/Main_Page.cs
+++ Assets/Script/Main_Page.cs
@@ -6,51 +6,56 @@ using UnityEngine.Networking;
 
 public class Main_Page : MonoBehaviour
 {
-    public Image menu_bar, main_bg, menu_bttn, basic_menu, main_bg_dark, cart_page;
-    public RawImage product;
-    public GameObject main_page, cart_scrollview;
+    public Image menu_bar, main_bg, menu_bttn, basic_menu, main_bg_dark, cart_page, product_Img;
+    public GameObject main_page, search_page, cart_scrollview, category_page, product_page;
     public Image mypage;
-    public GameObject[] product_page;
+    public GameObject pages;
     public GameObject curr_page;
     public GameObject[] cart_elements; //카트 요소들
     public ArrayList cart_content = new ArrayList(); //장바구니에 담긴 상품 리스트
-    public bool Is_menu_open;
-
+    bool Is_menu_open;
+    Animator animator;
     Stack<GameObject> page = new Stack<GameObject>();
 
+
     void Start() {
-        Is_menu_open = false;
+        //Is_menu_open = false;
         menu_bar.gameObject.SetActive(false);
         main_bg.gameObject.SetActive(true);
         menu_bttn.gameObject.SetActive(true);
         main_bg_dark.gameObject.SetActive(false);
         mypage.gameObject.SetActive(false);
         cart_page.gameObject.SetActive(false);
+        product_page.gameObject.SetActive(false);
        // get_Image();
         curr_page = main_page;
         main_page.SetActive(true);
+
     }
 
     //왼쪽 메뉴바 열기
-    public void menu_open() {
-        if (Is_menu_open)
-        {
-            menu_close();
-        }
-        else
-        {
+
+    public void menu_open()
+    {
+
+        animator = menu_bar.GetComponent<Animator>();
+        Is_menu_open = animator.GetBool("Is_menu_open");
+
+        animator.SetBool("Is_menu_open", !Is_menu_open);
             menu_bar.gameObject.SetActive(true);
-            main_bg_dark.gameObject.SetActive(true);
-            Is_menu_open = true;
-        }
-        }
+            //main_bg_dark.gameObject.SetActive(true);
+            //Is_menu_open = true;
+        
+    }
 
     //왼쪽 메뉴바 닫기
-    public void menu_close() {
-        menu_bar.gameObject.SetActive(false);
+    public void menu_close()
+    {
         main_bg_dark.gameObject.SetActive(false);
-        Is_menu_open = false;
+       // Is_menu_open = false;
     }
+
+
 
     //마이페이지 버튼 눌렀을 때
     public void my_page()
@@ -68,7 +73,37 @@ public class Main_Page : MonoBehaviour
     //상품 페이지로 이동하기
     public void go_product_detail()
     {
+        product_setting(gameObject.name);
+        change_page(product_page);
 
+        
+    }
+
+    //상품페이지 세팅
+    public void product_setting(string name)
+    {
+        product_Img.sprite = Resources.Load<Sprite>("1001_prod") ;
+
+    }
+
+    //카테고리 페이지로 이동하기
+    public void go_category_page()
+    {
+        menu_close();
+        change_page(category_page);
+    }
+
+    //첫메인 화면으로 이동하기
+    public void go_main_page()
+    {
+        change_page(main_page);
+    }
+
+
+    //검색페이지로 이동하기
+    public void go_search_page()
+    {
+        change_page(search_page);
     }
 
     //화면 전환
@@ -105,7 +140,7 @@ public class Main_Page : MonoBehaviour
     //상품 정보 얻기
     public void Get_prod_info()
     {
-      
+        
     }
 
 
